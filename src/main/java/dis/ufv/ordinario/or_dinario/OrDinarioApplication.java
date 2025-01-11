157a0011cb169575dd5b1f8ea420e15dd5575cb8
davidenrique <davidenriquea@icloud.com>
2025-01-11 12:33:49 +0100

    Commit inicial

---
+++ src/main/java/dis/ufv/ordinario/or_dinario/OrDinarioApplication.java
@@ -0,0 +1,13 @@
+package dis.ufv.ordinario.or_dinario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrDinarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrDinarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dis/ufv/ordinario/or_dinario/OrDinarioApplicationTests.java
@@ -0,0 +1,13 @@
+package dis.ufv.ordinario.or_dinario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrDinarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
