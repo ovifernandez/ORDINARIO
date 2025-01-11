ed16383c30cc34a02a6c6fc4dc4221f49caba273
davidenrique <davidenriquea@icloud.com>
2025-01-11 20:26:17 +0100

    Modal recoge los datos al doble clickar, botones no implementados.

--- skeleton-starter-flow-spring-24/src/main/java/org/vaadin/example/MainView.java
+++ skeleton-starter-flow-spring-24/src/main/java/org/vaadin/example/MainView.java
@@ -53,7 +53,44 @@ public class MainView extends VerticalLayout {
         grid.setItems(listaTurismos);
         grid.addClassName("grid-turismos");
         //Si dobleclikamos sobre un elemento de la lista:
+        grid.addItemDoubleClickListener(e->{
+            Turismo turismo = e.getItem();
+            Dialog dialog = new Dialog();
 
+            //Creamos los elementos que se añadiran al modal
+            TextField comunidadOrigen = new TextField("Comunidad Origen: ");
+            TextField provinciaOrigen = new TextField("Provincia Origen: ");
+            TextField comunidadDestino = new TextField("Comunidad Destino: ");
+            TextField provinciaDestino = new TextField("Provincia Destino: ");
+            TextField fechaInicio = new TextField("Fecha de Inicio: ");
+            TextField fechaFin = new TextField("Fecha de Fin: ");
+            IntegerField total = new IntegerField("Total: ");
+
+            dialog.setWidth("450px");
+            dialog.setCloseOnEsc(false);
+            dialog.setCloseOnOutsideClick(false);
+            String id = turismo.get_id();
+            dialog.setHeaderTitle("Detalles del Viaje");
+
+            comunidadOrigen.setValue(turismo.getOrigen().getComunidad());
+            provinciaOrigen.setValue(turismo.getOrigen().getProvincia());
+            comunidadDestino.setValue(turismo.getOrigen().getComunidad());
+            provinciaDestino.setValue(turismo.getDestino().getProvincia());
+            fechaInicio.setValue(turismo.getPeriodo().getFecha_inicio());
+            fechaFin.setValue(turismo.getPeriodo().getFecha_fin());
+            total.setValue(turismo.getTotal());
+            HorizontalLayout origenLayout = new HorizontalLayout(comunidadOrigen, provinciaOrigen);
+            HorizontalLayout destinoLayout = new HorizontalLayout(comunidadDestino, provinciaDestino);
+            HorizontalLayout fechasLayout = new HorizontalLayout(fechaInicio, fechaFin);
+            VerticalLayout dialogLayout = new VerticalLayout(origenLayout, destinoLayout, fechasLayout, total);
+            dialogLayout.setPadding(false);
+            dialogLayout.setSpacing(false);
+            dialogLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
+            dialogLayout.getStyle().set("width", "18rem").set("max-width", "100%");
+
+            dialog.add(dialogLayout);
+            dialog.open();
+        });
         Button cargarElems = new Button("Cargar Elems", e->{
             listaTurismos = service.getTurismos();
             grid.setItems(listaTurismos);
